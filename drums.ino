c4d3bc00e81cde1637b6c90a0252a5c4df3b9077
Jakub Zgolinski <jzgolinski@goodgamestudios.com>
2018-07-26 10:16:23 +0200

    hmmm

--- drums.ino
+++ drums.ino
@@ -1,99 +1,67 @@
 #include <MIDI.h>
-#include <Keypad.h>
 
 MIDI_CREATE_DEFAULT_INSTANCE();
 
-const byte ROWS = 4;
-const byte COLS = 4;
-char keys[ROWS][COLS] = {
-  {'1', '2', '3', '4'},
-  {'5', '6', '7', '8'},
-  {'9', 'a', 'b', 'c'},
-  {'d', 'e', 'f', 'g'}
-};
-byte rowPins[ROWS] = {5, 4, 3, 2};
-byte colPins[COLS] = {6, 7, 8, 9};
+const int CHANNELS = 4;
+const int THRESHOLD = 30;
+const int DURATION = 200;
 
-Keypad kpd = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+const int pins[CHANNELS] = {A0, A1, A2, A3};
+const int channels[CHANNELS] = {1, 2, 3, 4};
+int durations[CHANNELS] = {0, 0, 0, 0};
+unsigned long playing[CHANNELS] = {0, 0, 0, 0};
+
+boolean debug = false;
 
 void setup()
 {
-  MIDI.begin(MIDI_CHANNEL_OMNI);
+  MIDI.begin();
   Serial.begin(115200);
 }
 
 void loop()
 {
-  if (kpd.getKeys()) {
-    for (int i = 0; i < LIST_MAX; i++) {
-      if ( kpd.key[i].stateChanged ) {
-        switch (kpd.key[i].kstate) {
-          case PRESSED:
-            if (kpd.key[i].kchar == 'd') {
-              // kick
-              MIDI.sendNoteOn(72, 60, 1);
-            }
-            if (kpd.key[i].kchar == 'e') {
-              // snare
-              MIDI.sendNoteOn(72, 60, 2);
-            }
-            if (kpd.key[i].kchar == 'f') {
-              // closed hi-hat
-              MIDI.sendNoteOn(72, 60, 3);
-            }
-            if (kpd.key[i].kchar == 'g') {
-              // open hi-hat
-              MIDI.sendNoteOn(72, 60, 4);
-            }
-            if (kpd.key[i].kchar == '1') {
-              // sample 1
-              MIDI.sendNoteOn(72, 60, 5);
-            }
-            if (kpd.key[i].kchar == '2') {
-              // sample 2
-              MIDI.sendNoteOn(72, 60, 6);
-            }
-            if (kpd.key[i].kchar == '3') {
-              // sample 3
-              MIDI.sendNoteOn(72, 60, 7);
-            }
-            if (kpd.key[i].kchar == '4') {
-              // sample 4
-              MIDI.sendNoteOn(72, 60, 8);
-            }
-            break;
-          case HOLD:
-            break;
-          case RELEASED:
-            if (kpd.key[i].kchar == 'd') {
-              MIDI.sendNoteOff(72, 0, 1);
-            }
-            if (kpd.key[i].kchar == 'e') {
-              MIDI.sendNoteOn(72, 0, 2);
-            }
-            if (kpd.key[i].kchar == 'f') {
-              MIDI.sendNoteOn(72, 0, 3);
-            }
-            if (kpd.key[i].kchar == 'g') {
-              MIDI.sendNoteOn(72, 0, 4);
-            }
-            if (kpd.key[i].kchar == '1') {
-              MIDI.sendNoteOn(72, 0, 5);
-            }
-            if (kpd.key[i].kchar == '2') {
-              MIDI.sendNoteOn(72, 0, 6);
-            }
-            if (kpd.key[i].kchar == '3') {
-              MIDI.sendNoteOn(72, 0, 7);
-            }
-            if (kpd.key[i].kchar == '4') {
-              MIDI.sendNoteOn(72, 0, 8);
-            }
-            break;
-          case IDLE:
-            break;
-        }
+  int piezoVal = 0;
+  for (int i = 0; i < 3; i++) {
+    if (playing[i] == 0) {
+      piezoVal = analogRead(pins[i]);
+      if (piezoVal > THRESHOLD) {
+        noteOn(map(piezoVal, 0, 1023, 30, 127), i); 
       }
     }
+    if (playing[i] != 0 && (millis() - playing[i] >= DURATION)) {
+      noteOff(i);
+    }
   }
+}
+
+void noteOn(int velocity, int drum) {
+  MIDI.sendNoteOn(72, velocity, channels[drum]);
+  if (debug) {
+    Serial.print("noteOn at ");
+    Serial.print(channels[drum]);
+    Serial.print(" (");
+    Serial.print(velocity);
+    Serial.println(")");
+  }
+  playing[drum] = millis();
+}
+
+
+void noteOff(int drum) {
+  MIDI.sendNoteOff(72, 0, channels[drum]);
+  if (debug) {
+    Serial.print("noteOff at ");
+    Serial.println(channels[drum]);
+  }
+  playing[drum] = 0;
+}
+
+int getMaxVal(int lastVal, int pin) {
+  int currentVal = analogRead(pin);
+  while (currentVal > lastVal) {
+    lastVal = currentVal;
+    currentVal = analogRead(pin);
+  }
+  return lastVal;
 }
